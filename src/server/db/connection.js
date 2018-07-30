bc22ed1f389fb62f2a9888a165053f09bab19f73
James Evans <james3780@gmail.com>
2018-07-30 02:09:46 -0700

    revise environment syntax

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -1,7 +1,8 @@
 /*
 This file connects to the database using the appropriate Knex configuration based on the environment (development, test, staging, production, etc.)
 */
-const environment = process.env.NODE_ENV || 'development';
+const environment = 'development';
+// const environment = process.env.NODE_ENV || 'development';
 const config = require('../../../knexfile.js')[environment];
 
 module.exports = require('knex')(config);
