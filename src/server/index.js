49891cd17a9a07a8620b68322ec04ebf2d1db8b4
James Evans <james3780@gmail.com>
2019-11-23 16:44:47 -0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -127,7 +127,7 @@ setInterval(() => {
         console.log('Add New Price Success: ', resultAddPrice);
       }
   });
-}, 100000); // 21600000, Every 6 Hours | *15000000 Every 4.16 hours | 300K, Every 5 minutes | 150K, 2.5min 75K 1.25 min
+}, 8000000); // 21600000, Every 6 Hours | *15000000 Every 4.16 hours | 300K, Every 5 minutes | 150K, 2.5min 75K 1.25 min
 
 
 
