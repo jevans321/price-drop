442b983a691d8d400988ed1225a6d5bb42f7eed1
James Evans <james3780@gmail.com>
2019-11-23 18:32:00 -0800

    update package.json versions

--- src/server/index.js
+++ src/server/index.js
@@ -52,8 +52,6 @@ setInterval(() => {
     .useragent(useragents[getRandomInt(0, useragents.length - 1)])
     .goto('https://www.bestbuy.com/site/tvs/55-inch-tvs/pcmcat1514910111435.c?id=pcmcat1514910111435&qp=brand_facet%3DBrand~Samsung%5Ebrand_facet%3DBrand~Sony%5Ebrand_facet%3DBrand~VIZIO')
     .wait(2000)
-  
-    // scroll down page bit by bit to allow content to load
     .scrollTo(16000, 0)
     .wait(2000)
     .scrollTo(19000, 0)
@@ -173,7 +171,7 @@ setInterval(() => {
     .catch(error => {
       console.error('Scrape Error: ', error)
     })
-}, 15000000); // 21600000, Every 6 Hours | *15000000 Every 4.16 hours | 300K, Every 5 minutes | 150K, 2.5min 75K 1.25 min
+}, 100000); // 21600000, Every 6 Hours | *15000000 Every 4.16 hours | 300K, Every 5 minutes | 150K, 2.5min 75K 1.25 min
 
 
 
